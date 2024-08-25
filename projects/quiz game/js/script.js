b386a8069d45ae56318102dd9d21cac4d6373f36
AdeiaSaif <adeia2903saif@gmail.com>
2024-08-25 23:37:53 +0500

    Initial commit

---
+++ projects/quiz game/js/script.js
@@ -0,0 +1,16 @@
+function ask_ctg(){
+    document.getElementById("ctg").style.display="block";
+    document.getElementById("home").style.display="none";
+}
+function home(){
+    document.getElementById("ctg").style.display="none";
+    document.getElementById("home").style.display="block";
+}
+function sci(){
+    document.getElementById("ctg").style.display="none";
+    document.getElementById("sci").style.display="block";
+}
+function his(){
+    document.getElementById("ctg").style.display="none";
+    document.getElementById("sci").style.display="block";
+}
